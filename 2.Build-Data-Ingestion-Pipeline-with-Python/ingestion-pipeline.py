d1708eca491db4fdf487fd038a4fdd9b07b71d57
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-08-14 16:22:14 +0800

    2.Build-Data-Ingestion-Pipeline-with-Python

--- 2.Build-Data-Ingestion-Pipeline-with-Python/ingestion-pipeline.py
+++ 2.Build-Data-Ingestion-Pipeline-with-Python/ingestion-pipeline.py
@@ -1,1 +1,102 @@
 # here we write all the code to load all the source doucments and then chunk it up and embed the chunks and finally store in vector database
+import os
+from langchain_community.document_loaders import TextLoader, DirectoryLoader
+from langchain_unstructured import UnstructuredLoader # this helps us read text files or ppts
+from langchain_text_splitters import CharacterTextSplitter # help us chunk the documents
+from langchain_openai import OpenAIEmbeddings # embedding model to convert chunks into vector embeddings
+from langchain_chroma import Chroma # vector database
+from dotenv import load_dotenv
+
+load_dotenv() # load env variables
+
+# load files
+def load_documents(docs_path="docs"):
+    """Load all text files from the docs directory"""
+    print(f"Loading documents from {docs_path}...")
+    
+    # Check if docs directory exists
+    if not os.path.exists(docs_path):
+        raise FileNotFoundError(f"The directory {docs_path} does not exist. Please create it and add your company files.")
+    
+    # Load all .txt files from the docs directory with explicit UTF-8 encoding
+    loader = DirectoryLoader(
+        path=docs_path,
+        glob="*.txt",
+        loader_cls=TextLoader,
+        loader_kwargs={"encoding": "utf-8"}
+    )
+    
+    documents = loader.load()
+    
+    if len(documents) == 0:
+        raise FileNotFoundError(f"No .txt files found in {docs_path}. Please add your company documents.")
+    
+   
+    for i, doc in enumerate(documents[:2]):  # Show first 2 documents
+        print(f"\nDocument {i+1}:")
+        print(f"  Source: {doc.metadata['source']}")
+        print(f"  Content length: {len(doc.page_content)} characters")
+        print(f"  Content preview: {doc.page_content[:100]}...")
+        print(f"  metadata: {doc.metadata}")
+
+    return documents
+
+# chunk the files
+def split_documents(documents, chunk_size=1000, chunk_overlap=0):
+    """Split documents into smaller chunks with overlap"""
+    print("Splitting documents into chunks...")
+    
+    text_splitter = CharacterTextSplitter(
+        chunk_size=chunk_size, 
+        chunk_overlap=chunk_overlap
+    )
+    
+    chunks = text_splitter.split_documents(documents)
+    
+    if chunks:
+    
+        for i, chunk in enumerate(chunks[:5]):
+            print(f"\n--- Chunk {i+1} ---")
+            print(f"Source: {chunk.metadata['source']}")
+            print(f"Length: {len(chunk.page_content)} characters")
+            print(f"Content:")
+            print(chunk.page_content)
+            print("-" * 50)
+        
+        if len(chunks) > 5:
+            print(f"\n... and {len(chunks) - 5} more chunks")
+    
+    return chunks
+
+# embed the chunked files to vector database
+def create_vector_store(chunks, persist_directory="db/chroma_db"):
+    """Create and persist ChromaDB vector store"""
+    print("Creating embeddings and storing in ChromaDB...")
+        
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+    
+    # Create ChromaDB vector store
+    print("--- Creating vector store ---")
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory=persist_directory, 
+        collection_metadata={"hnsw:space": "cosine"}
+    )
+    print("--- Finished creating vector store ---")
+    
+    print(f"Vector store created and saved to {persist_directory}")
+    return vectorstore
+
+def main(): # define main function
+    print("Main Function")
+
+    #1. Loading the files
+    documents = load_documents(docs_path="docs")
+    #2. Chunking the files
+    chunks = split_documents(documents)
+    #3. Embedding and Storing in Vector DB
+    vectorStore = create_vector_store(chunks)
+
+if __name__ == "__main__":
+    main()
--- 2.Build-Data-Ingestion-Pipeline-with-Python/readme.md
+++ 2.Build-Data-Ingestion-Pipeline-with-Python/readme.md
@@ -1,1 +1,9 @@
 # python -m venv venv = create python virtual environment named "venv"
+
+# source venv/bin/activate = activate virtual environment
+
+# .\venv\Scripts\Activate.ps1 = activate virtual environment in powershell (windows)
+
+# source venv/Scripts/activate = activate virtual environment in gitbash (windows)
+
+# pip install langchain langchain-community langchain-text-splitters langchain-openai langchain-chroma python-dotenv
